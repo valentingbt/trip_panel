774653899803b93935e0a3b30ab499c3486e0ae4
Valentin Guibert <valentinguibert@hotmail.fr>
2019-03-22 09:57:15 +0100

    test assets files

--- public/javascripts/material.js
+++ public/javascripts/material.js
@@ -1,3 +1,5 @@
+alert("hey");
+
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, options);
