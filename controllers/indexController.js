3cc66f6534c9930f73e5be5c50def42281f7f13e
Valentin Guibert <valentinguibert@hotmail.fr>
2019-03-22 09:48:47 +0100

    Display Denver and Paris hour + Check/loading if done/undone

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 const Event = require('../models/event');
+const moment = require('moment-timezone');
 
 // HOME PAGE
 exports.index = function(req, res) {
@@ -8,7 +9,10 @@ exports.index = function(req, res) {
     .exec(function (err, list_events) {
         if (err) { return next(err); }
         //Successful, so render
-        res.render('index', { title_planning: 'Planning', event_list: list_events });
+        dateFrance = moment();
+        todayParis = dateFrance.tz('Europe/Paris').format('HH:mm');
+        todayDenver = moment().tz('America/Denver').format('HH:mm');
+        res.render('index', { title_planning: 'Planning', event_list: list_events, dateFrance: dateFrance, todayParis: todayParis, todayDenver: todayDenver });
     });
 
 };
