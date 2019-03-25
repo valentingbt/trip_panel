6fd7e815a174554b278daf84d85c0e09d3c80695
Valentin Guibert <valentinguibert@hotmail.fr>
2019-03-25 15:43:18 +0100

    minor update

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -12,7 +12,7 @@ exports.index = function(req, res) {
         dateFrance = moment();
         todayParis = dateFrance.tz('Europe/Paris').format('HH:mm');
         todayDenver = moment().tz('America/Denver').format('HH:mm');
-        res.render('index', { title_planning: 'Planning', event_list: list_events, dateFrance: dateFrance, todayParis: todayParis, todayDenver: todayDenver });
+        res.render('index', { title: 'Notre voyage aux USA : Tous les infos', title_planning: 'Planning', event_list: list_events, dateFrance: dateFrance, todayParis: todayParis, todayDenver: todayDenver });
     });
 
 };
